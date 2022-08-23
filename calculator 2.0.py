baa56ac218e797c58043e00575bb212e5859da72
Christian <chriaas@hiof.no>
2022-08-23 18:58:17 +0200

    Updated code to take two numbers instead and an arithmetic operator instead of listing every solution to the two numbers.

--- calculator 2.0.py
+++ calculator 2.0.py
@@ -1,23 +1,34 @@
-number_one = input("Type a number")
-number_two = input("type a second number")
+# Reworked calculator
 
-answer_1 = float(number_one) + float(number_two)
-answer_2 = float(number_one) - float(number_two)
-answer_3 = float(number_one) * float(number_two)
-answer_4 = float(number_one) / float(number_two)
-answer_5 = float(number_one) % float(number_two)
-answer_6 = float(number_one) ** float(number_two)
-answer_7 = float(number_one) // float(number_two)
+answer = input("Hi! Would you like to solve a math problem? yes/no: ")
+
+if answer.lower() == "yes":
+    number_one = input("Type a number: ")
+    math_symbol = input("Type the mathematic operator you want to use: ")
+    number_two = input("Type a number: ")
+    if math_symbol == "+":
+        print(number_one, math_symbol, number_two, "=", float(number_one) + float(number_two))
+    elif math_symbol == "-":
+        print(number_one, math_symbol, number_two, "=", float(number_one) - float(number_two))
+    elif math_symbol == "*":
+        print(number_one, math_symbol, number_two, "=", float(number_one) * float(number_two))
+    elif math_symbol == "/":
+        print(number_one, math_symbol, number_two, "=", float(number_one) / float(number_two))
+    elif math_symbol == "%":
+        print(number_one, math_symbol, number_two, "=", float(number_one) % float(number_two))
+    elif math_symbol == "**":
+        print(number_one, math_symbol, number_two, "=", float(number_one) ** float(number_two))
+    elif math_symbol == "//":
+        print(number_one, math_symbol, number_two, "=", float(number_one) // float(number_two))
+    else:
+        print("Operator is not supported.")
 
 
-print(f"Here are the answers:", ("Addition="),answer_1,
-                                ("Subtraction="),answer_2,
-                                ("Multiplication="),answer_3,
-                                ("Division="),answer_4,
-                                ("Modulus="), answer_5,
-                                ("Exponentiation="), answer_6,
-                                ("Floor division="),answer_7,)
 
-# Calculator 2.0 laget etter forslag fra studassistent. Finner ikke en ryddig måte å få printet hvilke mattematisk
-# løsning som blir brukt i stringformat.
+
+
+
+
+
+
 
